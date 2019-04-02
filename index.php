b3089b55adbd7080fdae2814df5afc3d53a26c5a
donaldrshade <donaldrshade@cedarville.edu>
2019-04-02 08:44:11 -0400

    Added file for linx machine.

---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0; URL='ProjectNope.html'" />
+    </head>
+    <p><a href="http://judah.cedarville.edu/~shade/TermProject/ProjectNope.html">Redirect</a></p>
+</html>
